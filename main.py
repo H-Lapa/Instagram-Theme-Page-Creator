66a44043070a445dddcbc48b2696c85fbccc41b8
Hugo Lapa <Hugolapa888@gmail.com>
2021-11-30 20:43:52 +0000

    menu change

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ class Myaccount:
 class Account:
     def __init__(self, username):
         self.username = username
+        self.link = "https://www.instagram.com/" + username + "/"
         self.posts_array = []
 
     def get_posts (self):
@@ -48,18 +49,32 @@ class Account:
         self.posts_array.append(post)
 
     def create_folder(self):
+        directory = self.username
+        path = os.path.join("Images", directory)
+        try:
+            os.makedirs(path, exist_ok = True)
+            print("Directory '%s' created successfully" % directory)
+        except OSError as error:
+            print("Directory '%s' can not be created" % directory)
  
 
-class Posts:
+class Post:
     def __init__(self, link, date):
         self.link = link
         self.date = date # can use datetime liabry
         self.caption = ""
 
+    def download(self, name):
+        urllib.request.urlretrieve(self.link, "Images/" + name +"/" + currentdateandtimeposted + ".png")
+
 
 class Menu:
     def __init__(self):
         self.text = "-------------------- \n     1.Execute \n   2.Add Accounts \n    3.Exit code \n     4.Resume \n    5.Login"
+
+    def logged_in(self, username):
+        self.text = "-------------------- \n     1.Execute \n   2.Add Accounts \n    3.Exit code \n     4.Resume \n    Logged in as" + username
+
 
 
 class Simulation:
@@ -70,7 +85,7 @@ class Simulation:
     def run (self):
         pass
 
-    
+
 
 
 
