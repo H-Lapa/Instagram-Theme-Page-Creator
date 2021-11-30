36bcbf1ad5e27506de9beecd8ae1b8a5641ce10a
Hugo Lapa <Hugolapa888@gmail.com>
2021-11-30 20:21:03 +0000

    MyAccount class

--- main.py
+++ main.py
@@ -24,6 +24,11 @@
 #download and upload all the new posts
 
 #func.createFolders(func.setUp())
+class Myaccount:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
 
 
 class Account:
@@ -43,14 +48,18 @@ class Posts:
         self.date = date # can use datetime liabry
 
 
-
 class Menu:
     def __init__(self):
-        self.text = "-------------------- \n     1.Execute \n   2.Add Accounts \n    3.Exit code \n     4.Resume"
+        self.text = "-------------------- \n     1.Execute \n   2.Add Accounts \n    3.Exit code \n     4.Resume \n    5.Login"
 
 
-class simulation:
+class Simulation:
     def __init__(self):
+        """Run Insta Account Simulation"""
+        pass
+
+    def run (self):
+        pass
 
 
 
