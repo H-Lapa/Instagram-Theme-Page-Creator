3e1dc97dd4ae2644e078885701639480032af2bf
Hugo Lapa <Hugolapa888@gmail.com>
2021-12-23 11:07:56 +0000

    integrated into account class

--- Account.py
+++ Account.py
@@ -54,12 +54,57 @@ class Account:
             writer = csv.writer(writeFile)
             writer.writerows(lines)
 
-
     def set_date(self, date):
         self.latest_post_date = date
         return
 
     def fetch_posts(self, driver):
+        driver.get(self.link)
+        time.sleep(5)
+        end = False
+        row = 1
+        while (end):
+            intial_path = f"/html/body/div[1]/section/main/div/div[2]/article/div[1]/div/div[{row}]"
+
+            try:
+                for element_in_array in range(1, 4):
+                    #gets the image url, which can be used to download later
+                    path = intial_path + f"/div[ {element_in_array} ]/a/div/div[1]/img"
+                    var = driver.find_element_by_xpath(path)
+                    print(var.get_attribute("src"))
+
+                    #Element to be clicked to open the image to full width
+                    image_button = driver.find_element_by_xpath( intial_path + f"/div[{element_in_array}]/a")
+                    image_button.send_keys(Keys.RETURN)
+
+                    #finds the exit button 
+                    exitbutton = driver.find_element_by_css_selector(".BI4qX > button:nth-child(1)")
+                    time.sleep(3)
+
+                    #gets the datetime from the element to late be converted to datetime object
+                    variabletime = driver.find_element_by_css_selector("._1o9PC").get_attribute("datetime")
+                    exitbutton.send_keys(Keys.RETURN)
+                    time.sleep(2)
+                    #prints datetime to know if ive got it
+                    print(variabletime)
+                    #turns the date string into a datetime object
+                    variabletime = datetime.strptime(variabletime, "%Y-%m-%dT%H:%M:%f.000z")
+
+                    if self.datetime == variabletime:
+                        #end the for loop at this position
+                        #increment to last value of loop to end it?
+                        element_in_array = 4
+                        end = True
+                        #also end the while loop setting end to true
+                    else:
+                        append(Post(var.get_attribute("src"), variabletime))
+                        row += 1
+            except:
+                #scroll down element not found
+                pass
+
+        set_date(self.posts_array[0].date)
+
         #go through the posts until 
         #one has a date before or equal to the date time of latest date post
 
@@ -71,7 +116,6 @@ class Account:
         #append posts which are before to the latest date post
         #once all have been added to the array
         #set the latest date post to the date of the post in 0 index
-        driver.get(self.link)
         
                 
 
--- main.py
+++ main.py
@@ -45,49 +45,6 @@ class Simulation:
         # self.account_array = self.create_account_array(list, driver)
         #self.set_intial_dates()
 
-        driver.get("https://www.instagram.com/bot_goose/")
-        time.sleep(5)
-        end = False
-        #loop through elements in first row
-        row = 1
-        while (end):
-            intial_path = f"/html/body/div[1]/section/main/div/div[2]/article/div[1]/div/div[{row}]"
-
-            try:
-                for element_in_array in range(1, 4):
-                    #gets the image url, which can be used to download later
-                    path = intial_path + f"/div[ {element_in_array} ]/a/div/div[1]/img"
-                    var = driver.find_element_by_xpath(path)
-                    print(var.get_attribute("src"))
-
-                    #Element to be clicked to open the image to full width
-                    image_button = driver.find_element_by_xpath( intial_path + f"/div[{element_in_array}]/a")
-                    image_button.send_keys(Keys.RETURN)
-
-                    #finds the exit button 
-                    exitbutton = driver.find_element_by_css_selector(".BI4qX > button:nth-child(1)")
-                    time.sleep(3)
-
-                    #gets the datetime from the element to late be converted to datetime object
-                    variabletime = driver.find_element_by_css_selector("._1o9PC").get_attribute("datetime")
-                    exitbutton.send_keys(Keys.RETURN)
-                    time.sleep(2)
-                    #prints datetime to know if ive got it
-                    print(variabletime)
-                    #turns the date string into a datetime object
-                    variabletime = datetime.strptime(variabletime, "%Y-%m-%dT%H:%M:%f.000z")
-
-                    if self.datetime == variabletime:
-                        #end the for loop at this position
-                        #increment to last value of loop to end it?
-
-                        #also end the while loop setting end to true
-                        pass
-            except:
-                #scroll down element not found
-                pass
-
-            row += 1
 
 
         #once account array is made
