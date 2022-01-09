56245fa4049ba90c33b2dd1c932558b674fa19ba
Hugo Lapa <Hugolapa888@gmail.com>
2022-01-09 15:51:40 +0000

    get_input for menu

--- Menu.py
+++ Menu.py
@@ -5,6 +5,11 @@ class Menu:
     def get_menu(self):
         return self.text
 
+    def get_input(self):
+        self.get_menu()
+        response = int(input("Input: "))
+        return response
+
     def logged_in(self, username):
         self.text = "-------------------- \n     1.Execute \n   2.Add Accounts \n    3.Exit code \n     4.Resume \n    Logged in as" + username
 
